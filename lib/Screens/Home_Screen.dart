ad87db67fa5159dfb16f7c648142a02a1b1eefa3
sagarpatel115 <ps157615@gmail.com>
2022-06-05 22:42:34 +0530

    added connectivity functionality

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -1,11 +1,42 @@
+
+import 'package:bloc_check_internet_connection/blocs/internet_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/internet_state.dart';
+
 
 class HomeScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
-      body: SafeArea(child: Container()),
+      body: SafeArea(child: Center(
+      child:
+      BlocConsumer<InternetBloc, InternetState>(
+        listener: (context, state) {
+        if(state is InternetGainedState){
+             ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Internet Connected"),backgroundColor: Colors.green,));
+          }else if(state is InternetLostState){
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("No Internet Connection"),backgroundColor: Colors.red,));
+          }else{
+             //return Text('loading....');
+          }
+        },
+        builder: (context, state) {
+          if(state is InternetGainedState){
+              return Text('Connected');
+          }else if(state is InternetLostState){
+              return Text('Not Connected');
+          }else{
+             return Text('loading....');
+          }
+        },
+      )
+      
+      )),
     );
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:bloc_check_internet_connection/Screens/Home_Screen.dart';
+import 'package:bloc_check_internet_connection/blocs/internet_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -9,12 +12,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child:MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home:  HomeScreen(),
+    ),
     );
   }
 }
